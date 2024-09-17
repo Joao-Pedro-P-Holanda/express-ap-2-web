ac3f38ad18336057d2d730dd55f19c5a92392095
Joao-Pedro-P-Holanda <joaopedroph.dev@gmail.com>
2024-09-17 15:39:17 -0300

    cors

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 require("dotenv").config();
 require("./db");
+
+const cors = require("cors");
 const express = require("express");
 const alunoRouter = require("./routes/aluno");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+
 app.use("/aluno", alunoRouter);
 
 app.get("/", (req, res) => {
